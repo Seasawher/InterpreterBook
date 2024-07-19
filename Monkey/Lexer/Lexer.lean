345db4570739b94cf59d7cb01194b3e1cc548e25
Seasawher <tellagoodstory@icloud.com>
2024-07-19 22:09:30 +0900

    ゼミによる進捗

--- Monkey/Lexer/Lexer.lean
+++ Monkey/Lexer/Lexer.lean
@@ -1,9 +1,51 @@
+import Monkey.Token.Token
+
 structure Lexer where
   input : String
   position : Nat := 0
   readPosition : Nat := 0
-  ch : UInt8 := 0
+  ch : Char := '\x00'
 deriving Repr
 
-def Lexer.new (input : String) (position readPosition : Nat := 0) (ch : UInt8 := 0) : Lexer :=
-  { input := input, position := position, readPosition := readPosition, ch := ch }
+-- 空文字を表す Char
+#guard ⟨0, by decide⟩ = '\x00'
+
+-- アルファベットかどうか判定する
+#check Char.isAlpha
+
+namespace Lexer
+
+def readChar (l : Lexer) : Lexer :=
+  let l' := if l.readPosition ≥ l.input.length
+    then { l with ch := '\x00' }
+    else { l with ch := l.input.get ⟨l.readPosition⟩}
+  { l' with position := l.readPosition, readPosition := l.readPosition + 1 }
+
+def new (input : String) (position readPosition : Nat := 0) (ch : Char := '\x00') : Lexer := Id.run do
+  let mut l := { input := input, position := position, readPosition := readPosition, ch := ch }
+  l := l.readChar
+  return l
+
+open TokenType
+
+def nextToken : StateM Lexer Token := do
+  let mut l ← get
+  let tok := match l.ch with
+    | '=' => Token.mk ASSIGN (String.singleton l.ch)
+    | '+' => Token.mk PLUS (String.singleton l.ch)
+    | '(' => Token.mk LPAREN (String.singleton l.ch)
+    | ')' => Token.mk RPAREN (String.singleton l.ch)
+    | '{' => Token.mk LBRACE (String.singleton l.ch)
+    | '}' => Token.mk RBRACE (String.singleton l.ch)
+    | ',' => Token.mk COMMA (String.singleton l.ch)
+    | ';' => Token.mk SEMICOLON (String.singleton l.ch)
+    | '\x00' => Token.mk EOF ""
+    | _ =>
+      if l.ch.isAlpha then
+        sorry
+      else
+        Token.mk ILLEGAL (String.singleton l.ch)
+  set l.readChar
+  return tok
+
+end Lexer
--- Monkey/Lexer/LexerTest.lean
+++ Monkey/Lexer/LexerTest.lean
@@ -1,29 +1,78 @@
 import Monkey.Lexer.Lexer
 import Monkey.Token.Token
 
-def Lexer.nextToken (l : Lexer) : Token := sorry
-
 open TokenType
 
-def testNextToken : IO Unit := do
-  let input := "=+(){},;"
-  let tests : Array (TokenType × String) := #[
-    (ASSIGN, "="),
-    (PLUS, "+"),
-    (LPAREN, "("),
-    (RPAREN, ")"),
-    (LBRACE, "{"),
-    (RBRACE, "}"),
-    (COMMA, ","),
-    (SEMICOLON, ";"),
-    (EOF, "")
-  ]
-  let l : Lexer := Lexer.new input
-  for tt in tests do
-    let tok := l.nextToken
+def testNextToken (input : String) (expected : Array (TokenType × String)) : IO Unit := do
+  let mut l : Lexer := Lexer.new input
+  for tt in expected do
+    let ⟨tok, l'⟩ := l.nextToken |>.run
+    l := l'
     if tok.type ≠ tt.fst then
       throw <| .userError s!"tests failed: - tokentype wrong. expected={tt.fst}, got={tok.type}"
     if tok.literal ≠ tt.snd then
       throw <| .userError s!"tests failed: - literal wrong. expected={tt.snd}, got={tok.literal}"
 
   IO.println s!"ok!"
+
+-- #eval testNextToken
+--   (input := "=+(){},;")
+--   (expected := #[
+--     (ASSIGN, "="),
+--     (PLUS, "+"),
+--     (LPAREN, "("),
+--     (RPAREN, ")"),
+--     (LBRACE, "{"),
+--     (RBRACE, "}"),
+--     (COMMA, ","),
+--     (SEMICOLON, ";"),
+--     (EOF, "")
+--   ])
+
+-- #eval testNextToken
+--   (input := "let five = 5;
+--   let ten = 10;
+--   let add = fn(x, y) {
+--   x + y;
+--   };
+--   let result = add(five, ten);
+--   ")
+--   (expected := #[
+--     (LET, "let"),
+--     (IDENT, "five"),
+--     (ASSIGN, "="),
+--     (INT, "5"),
+--     (SEMICOLON, ";"),
+--     (LET, "let"),
+--     (IDENT, "ten"),
+--     (ASSIGN, "="),
+--     (INT, "10"),
+--     (SEMICOLON, ";"),
+--     (LET, "let"),
+--     (IDENT, "add"),
+--     (ASSIGN, "="),
+--     (FUNCTION, "fn"),
+--     (LPAREN, "("),
+--     (IDENT, "x"),
+--     (COMMA, ","),
+--     (IDENT, "y"),
+--     (RPAREN, ")"),
+--     (LBRACE, "{"),
+--     (IDENT, "x"),
+--     (PLUS, "+"),
+--     (IDENT, "y"),
+--     (SEMICOLON, ";"),
+--     (RBRACE, "}"),
+--     (SEMICOLON, ";"),
+--     (LET, "let"),
+--     (IDENT, "result"),
+--     (ASSIGN, "="),
+--     (IDENT, "add"),
+--     (LPAREN, "("),
+--     (IDENT, "five"),
+--     (COMMA, ","),
+--     (IDENT, "ten"),
+--     (RPAREN, ")"),
+--     (SEMICOLON, ";"),
+--     (EOF, "")
+--   ])
