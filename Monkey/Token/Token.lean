984f31bba0dcc47a4478e313d265f4f94cedc0a3
Seasawher <tellagoodstory@icloud.com>
2024-08-16 21:54:08 +0900

    TokenType を削除する

--- Monkey/Token/Token.lean
+++ Monkey/Token/Token.lean
@@ -1,91 +1,5 @@
 import Lean.Data.HashMap
 
-/-- Token の種類 -/
-inductive TokenType where
-  /-- 受け入れ不能エラー -/
-  | ILLEGAL
-  /-- ファイル終端 -/
-  | EOF
-  /-- 識別子 -/
-  | IDENT
-  /-- 整数 -/
-  | INT
-  /-- 代入記号 "=" -/
-  | ASSIGN
-  /-- 足し算記号 + -/
-  | PLUS
-  /-- コンマ , -/
-  | COMMA
-  /-- セミコロン ; -/
-  | SEMICOLON
-  /-- 開き括弧 ( -/
-  | LPAREN
-  /-- 閉じ括弧 ) -/
-  | RPAREN
-  /-- 開き波括弧 -/
-  | LBRACE
-  /-- 閉じ波括弧 -/
-  | RBRACE
-  /-- 無名関数 fn -/
-  | FUNCTION
-  /-- LET キーワード -/
-  | LET
-  /-- 引き算記号 "-" -/
-  | MINUS
-  /-- ビックリマーク ! -/
-  | BANG
-  /-- アスタリスク * -/
-  | ASTERISK
-  /-- スラッシュ "/" -/
-  | SLASH
-  /-- 小なり "<" -/
-  | LT
-  /-- 大なり ">" -/
-  | GT
-  /-- true : Bool -/
-  | TRUE
-  /-- false : Bool -/
-  | FALSE
-  /-- IF キーワード -/
-  | IF
-  /-- ELSE キーワード -/
-  | ELSE
-  /-- RETURN キーワード -/
-  | RETURN
-deriving Repr, DecidableEq
-
-/-- TokenType を文字列に変換する -/
-def TokenType.toString (t : TokenType) : String :=
-  match t with
-  | .ILLEGAL => "ILLEGAL"
-  | .EOF => "EOF"
-  | .IDENT => "IDENT"
-  | .INT => "INT"
-  | .ASSIGN => "="
-  | .PLUS => "+"
-  | .COMMA => ","
-  | .SEMICOLON => ";"
-  | .LPAREN => "("
-  | .RPAREN => ")"
-  | .LBRACE => "{"
-  | .RBRACE => "}"
-  | .FUNCTION => "FUNCTION"
-  | .LET => "LET"
-  | .MINUS => "-"
-  | .BANG => "!"
-  | .ASTERISK => "*"
-  | .SLASH => "/"
-  | .LT => "<"
-  | .GT => ">"
-  | .TRUE => "TRUE"
-  | .FALSE => "FALSE"
-  | .IF => "IF"
-  | .ELSE => "ELSE"
-  | .RETURN => "RETURN"
-
-instance : ToString TokenType where
-  toString := TokenType.toString
-
 /-- トークン
 
 本にある Token の定義では structure になっていたので、
@@ -95,7 +9,7 @@ inductive Token where
   /-- 識別子 -/
   | IDENT (name : String)
   /-- 数値リテラル -/
-  | INT (value : String)
+  | INT (value : Int)
   /-- 受け入れ不能エラー -/
   | ILLEGAL
   /-- ファイル終端 -/
@@ -144,11 +58,43 @@ inductive Token where
   | RETURN
 deriving Repr, BEq, DecidableEq
 
-open TokenType Lean
+/-- Token を文字列に変換する -/
+def Token.toString (t : Token) : String :=
+  match t with
+  | .ILLEGAL => "ILLEGAL"
+  | .EOF => "EOF"
+  | .IDENT lit => lit
+  | .INT lit => ToString.toString lit
+  | .ASSIGN => "="
+  | .PLUS => "+"
+  | .COMMA => ","
+  | .SEMICOLON => ";"
+  | .LPAREN => "("
+  | .RPAREN => ")"
+  | .LBRACE => "{"
+  | .RBRACE => "}"
+  | .FUNCTION => "FUNCTION"
+  | .LET => "LET"
+  | .MINUS => "-"
+  | .BANG => "!"
+  | .ASTERISK => "*"
+  | .SLASH => "/"
+  | .LT => "<"
+  | .GT => ">"
+  | .TRUE => "TRUE"
+  | .FALSE => "FALSE"
+  | .IF => "IF"
+  | .ELSE => "ELSE"
+  | .RETURN => "RETURN"
 
-/-- 言語のキーワード -/
-def keywords : HashMap String TokenType :=
-  let list : List (String × TokenType) := [
+instance : ToString Token where
+  toString := Token.toString
+
+open Lean Token
+
+/-- 言語のキーワードを格納する辞書 -/
+def keywords : HashMap String Token :=
+  let list : List (String × Token) := [
     ("fn", FUNCTION),
     ("let", LET),
     ("true", TRUE),
@@ -160,7 +106,7 @@ def keywords : HashMap String TokenType :=
   HashMap.ofList list
 
 /-- ユーザ定義の識別子なのか、言語のキーワードなのか分類する -/
-def LookupIdent (ident : String) : TokenType :=
+def LookupIdent (ident : String) : Token :=
   match keywords.find? ident with
   | some tok => tok
-  | none => IDENT
+  | none => IDENT ident
