3988cc656ca13dba6c13c02e49779b9217d88dbb
Seasawher <tellagoodstory@icloud.com>
2024-10-11 23:09:28 +0900

    未実装の部分を notimplemented で表す

--- Monkey/Ast/Ast.lean
+++ Monkey/Ast/Ast.lean
@@ -5,29 +5,35 @@ inductive Expression where
   /-- 識別子 -/
   | identifier (token : Token) (value : String) : Expression
 
+  /-- `Expression` の未実装の部分を表す -/
+  | notImplemented
+
 deriving Repr, DecidableEq
 
 /-- Expression を文字列に変換する -/
 def Expression.tokenLiteral (e : Expression) : String :=
   match e with
   | .identifier token _ => s!"{token}"
+  | .notImplemented => default
 
 /-- 文。本文とは異なる実装を採用しており、`statementNode()` に相当するものは不要。-/
 inductive Statement where
   /-- let 文 -/
   | letStmt (token : Token) (name : String) (value : Expression) : Statement
 
+  /-- Statement の未実装の部分を表す -/
+  | notImplemented
+
 deriving Repr, DecidableEq
 
 instance : ToString Statement where
   toString s := reprStr s
 
-#check Repr.reprPrec
-
 /-- Statement の ToString 関数に相当するもの -/
 def Statement.tokenLiteral (s : Statement) : String :=
   match s with
   | .letStmt token _ _ => s!"{token}"
+  | .notImplemented => default
 
 /-- AST のノード -/
 inductive Node where
@@ -40,12 +46,8 @@ inductive Node where
 /-- プログラムを文の集まりとして定義する -/
 abbrev Program := List Statement
 
--- deriving DecidableEq, Repr
-
 /-- Program の ToString 関数に相当するもの -/
 def Program.tokenLiteral (p : Program) : String :=
   match p with
   | [] => ""
   | p :: _ => p.tokenLiteral
-
--- instance : ToString Program := inferInstance
